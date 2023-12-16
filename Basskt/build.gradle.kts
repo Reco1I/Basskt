09b9aa2954f335aee06917a6cd9458318b1f0df1
Reco1l <geronimoferruccio15@gmail.com>
2023-12-15 22:43:52 -0300

    Remove unnecessary dependencies

--- Basskt/build.gradle.kts
+++ Basskt/build.gradle.kts
@@ -33,16 +33,6 @@ android {
     }
 }
 
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
-
 publishing {
     publications {
         register<MavenPublication>("release") {
